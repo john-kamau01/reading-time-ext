da247ce850e5da4ed49c0194887ec5798b0865f8
John Waweru <johnwaweru092@gmail.com>
2023-04-28 16:51:37 +0300

    Added my project

---
+++ scripts/content.js
@@ -0,0 +1,22 @@
+const article = document.querySelector("article");
+
+if (article) {
+  const text = article.textContent;
+  const wordMatchRegExp = /[^\s]+/g; //Regular Expression
+  const words = text.matchAll(wordMatchRegExp);
+
+  // matchAll returns an iterator, convert to array to get word count
+  const wordCount = [...words].length;
+  const readingTime = Math.round(wordCount / 200);
+  const badge = document.createElement("p");
+
+  badge.classList.add("color-secondarytext", "type--caption");
+  badge.textContent = `⏱️ ${readingTime} min read`;
+
+  // Support for API reference docs
+  const heading = article.querySelector("h1");
+  // Support for article docs with date
+  const date = article.querySelector("time")?.parentNode;
+
+  (date ?? heading).insertAdjacentElement("afterend", badge);
+}
